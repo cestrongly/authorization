feaa6730fc04cc01340cbfd3a53cb7b9d25053d6
cestrmail@163.com <cestrmail@163.com>
2022-08-30 18:13:10 +0800

    rsa 签名加密解密

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,12 +1,46 @@
 <!--
  * @Author: cest
  * @Date: 2022-08-30 10:18:36
- * @LastEditTime: 2022-08-30 15:40:02
+ * @LastEditTime: 2022-08-30 18:10:01
  * @LastEditors: cest
  * @FilePath: /authorization/src/pages/login/index.vue
  * @Description: 登录页面
 -->
 <script lang="ts" setup>
+import CryptoJS from 'crypto-js'
+import JSEncrypt from '~/utils/jsencrypt/lib/index'
+const msg = '123'
+// 我的私钥给你签名
+const myPrivateKey = `MIICXQIBAAKBgQDlOJu6TyygqxfWT7eLtGDwajtNFOb9I5XRb6khyfD1Yt3YiCgQ
+WMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76xFxdU6jE0NQ+Z+zEdhUTooNR
+aY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4gwQco1KRMDSmXSMkDwIDAQAB
+AoGAfY9LpnuWK5Bs50UVep5c93SJdUi82u7yMx4iHFMc/Z2hfenfYEzu+57fI4fv
+xTQ//5DbzRR/XKb8ulNv6+CHyPF31xk7YOBfkGI8qjLoq06V+FyBfDSwL8KbLyeH
+m7KUZnLNQbk8yGLzB3iYKkRHlmUanQGaNMIJziWOkN+N9dECQQD0ONYRNZeuM8zd
+8XJTSdcIX4a3gy3GGCJxOzv16XHxD03GW6UNLmfPwenKu+cdrQeaqEixrCejXdAF
+z/7+BSMpAkEA8EaSOeP5Xr3ZrbiKzi6TGMwHMvC7HdJxaBJbVRfApFrE0/mPwmP5
+rN7QwjrMY+0+AbXcm8mRQyQ1+IGEembsdwJBAN6az8Rv7QnD/YBvi52POIlRSSIM
+V7SwWvSK4WSMnGb1ZBbhgdg57DXaspcwHsFV7hByQ5BvMtIduHcT14ECfcECQATe
+aTgjFnqE/lQ22Rk0eGaYO80cc643BXVGafNfd9fcvwBMnk0iGX0XRsOozVt5Azil
+psLBYuApa66NcVHJpCECQQDTjI2AQhFc1yRnCU/YgDnSpJVm1nASoRUnU8Jfm3Oz
+uku7JUXcVpt08DFSceCEX9unCuMcT72rAQlLpdZir876`
+// 发送我的公钥给你签字认领
+const myPublicKey = `MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDlOJu6TyygqxfWT7eLtGDwajtN
+FOb9I5XRb6khyfD1Yt3YiCgQWMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76
+xFxdU6jE0NQ+Z+zEdhUTooNRaY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4
+gwQco1KRMDSmXSMkDwIDAQAB`
+// 用你的公钥，加密消息
+const yourPublicKey = ''
+
+console.log('JSEncrypt', JSEncrypt)
+
+const rsa = new JSEncrypt()
+// 第一步加密 - 用你的公钥加密
+// 第二步签名 - 用我的私钥签名
+// 第三步发送 - 签名的消息和我的公钥
+rsa.setPrivateKey(myPrivateKey)
+const signature = rsa.sign(msg, CryptoJS.SHA256, 'sha256')
+console.log('signature:', signature)
 const form = ref({
   name: '',
   password: '',
--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,9 +1,9 @@
 /*
  * @Author: cest
  * @Date: 2022-08-18 13:57:11
- * @LastEditTime: 2022-08-18 14:39:47
+ * @LastEditTime: 2022-08-30 17:43:51
  * @LastEditors: cest
- * @FilePath: /color-ui-next/src/shims.d.ts
+ * @FilePath: /authorization/src/shims.d.ts
  * @Description: 腻子脚本 类型声明文件
  */
 declare interface Window {
@@ -27,3 +27,5 @@ declare module '*.vue' {
 // declare module 'element-plus/lib/locale/lang/zh-cn'
 // declare module 'element-plus/lib/locale/lang/en'
 declare module '@intlify/vite-plugin-vue-i18n'
+declare module 'js-crypto-rsa'
+declare module 'crypto-js'
