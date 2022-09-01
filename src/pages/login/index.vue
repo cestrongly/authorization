bbdd27259112db2afc1dd6f3dc1da2de08e670dc
cestrmail@163.com <cestrmail@163.com>
2022-09-01 08:27:35 +0800

    rsa 非对称加密算法联通

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: cest
  * @Date: 2022-08-30 10:18:36
- * @LastEditTime: 2022-08-30 18:10:01
+ * @LastEditTime: 2022-08-31 11:40:19
  * @LastEditors: cest
  * @FilePath: /authorization/src/pages/login/index.vue
  * @Description: 登录页面
@@ -9,38 +9,44 @@
 <script lang="ts" setup>
 import CryptoJS from 'crypto-js'
 import JSEncrypt from '~/utils/jsencrypt/lib/index'
-const msg = '123'
-// 我的私钥给你签名
-const myPrivateKey = `MIICXQIBAAKBgQDlOJu6TyygqxfWT7eLtGDwajtNFOb9I5XRb6khyfD1Yt3YiCgQ
-WMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76xFxdU6jE0NQ+Z+zEdhUTooNR
-aY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4gwQco1KRMDSmXSMkDwIDAQAB
-AoGAfY9LpnuWK5Bs50UVep5c93SJdUi82u7yMx4iHFMc/Z2hfenfYEzu+57fI4fv
-xTQ//5DbzRR/XKb8ulNv6+CHyPF31xk7YOBfkGI8qjLoq06V+FyBfDSwL8KbLyeH
-m7KUZnLNQbk8yGLzB3iYKkRHlmUanQGaNMIJziWOkN+N9dECQQD0ONYRNZeuM8zd
-8XJTSdcIX4a3gy3GGCJxOzv16XHxD03GW6UNLmfPwenKu+cdrQeaqEixrCejXdAF
-z/7+BSMpAkEA8EaSOeP5Xr3ZrbiKzi6TGMwHMvC7HdJxaBJbVRfApFrE0/mPwmP5
-rN7QwjrMY+0+AbXcm8mRQyQ1+IGEembsdwJBAN6az8Rv7QnD/YBvi52POIlRSSIM
-V7SwWvSK4WSMnGb1ZBbhgdg57DXaspcwHsFV7hByQ5BvMtIduHcT14ECfcECQATe
-aTgjFnqE/lQ22Rk0eGaYO80cc643BXVGafNfd9fcvwBMnk0iGX0XRsOozVt5Azil
-psLBYuApa66NcVHJpCECQQDTjI2AQhFc1yRnCU/YgDnSpJVm1nASoRUnU8Jfm3Oz
-uku7JUXcVpt08DFSceCEX9unCuMcT72rAQlLpdZir876`
-// 发送我的公钥给你签字认领
-const myPublicKey = `MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDlOJu6TyygqxfWT7eLtGDwajtN
-FOb9I5XRb6khyfD1Yt3YiCgQWMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76
-xFxdU6jE0NQ+Z+zEdhUTooNRaY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4
-gwQco1KRMDSmXSMkDwIDAQAB`
-// 用你的公钥，加密消息
-const yourPublicKey = ''
+const msg = 'hello world'
 
-console.log('JSEncrypt', JSEncrypt)
-
+// 生成我的秘钥
 const rsa = new JSEncrypt()
+
+// 我的私钥给你签名
+const myPrivateKey = rsa.getPrivateKey()
+  .replace('-----BEGIN RSA PRIVATE KEY-----', '')
+  . replace('-----END RSA PRIVATE KEY-----', '')
+// 发送我的公钥给你签字认领
+const myPublicKey = rsa.getPublicKey()
+  .replace('-----BEGIN PUBLIC KEY-----', '')
+  .replace('-----END PUBLIC KEY-----', '')
+// 用你的公钥，加密消息
+const yourPublicKey = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3lcNOhUwySR1ONlugEKdFNmnaCPJWipx0OrUgOpdIseBc99P8aQg+ziSkmyQ7V8MnyVL5NwRH5Y3Kj9eRY3dTG0y0EtV1V3O7KUSW/DyhnqB8ixWl8UCYQUtaBImkl0wKaycvBPePSSpI4Wa288xYPxJWfzYy/LLWOBE8PkyH8zeRVQOqiVitAOd+ohQ8n6g94o753v7m9qa02TA/Q+Qjv/XyS592AlR7jqBJhpW9AWBGdx8Z2bj9nrbmxt0K7ba03IYRn1hgNr3M/E7S196+jW5gQR88uBMHygMaxJHK0KVviqAInjJeixA3FaQEl73iG2uWBu5Q6g+K0m8eH0PiwIDAQAB'
+const yourPrivateKey = 'MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDeVw06FTDJJHU42W6AQp0U2adoI8laKnHQ6tSA6l0ix4Fz30/xpCD7OJKSbJDtXwyfJUvk3BEfljcqP15Fjd1MbTLQS1XVXc7spRJb8PKGeoHyLFaXxQJhBS1oEiaSXTAprJy8E949JKkjhZrbzzFg/ElZ/NjL8stY4ETw+TIfzN5FVA6qJWK0A536iFDyfqD3ijvne/ub2prTZMD9D5CO/9fJLn3YCVHuOoEmGlb0BYEZ3HxnZuP2etubG3QrttrTchhGfWGA2vcz8TtLX3r6NbmBBHzy4EwfKAxrEkcrQpW+KoAieMl6LEDcVpASXveIba5YG7lDqD4rSbx4fQ+LAgMBAAECggEAMIqI1LlzoTZYcUTnHxFsAfVS2yWU/B5wItBh/JMuKj2sQYPrjDHtEWvUdCtHqMsrkkMuQpTZGDBPcuXKyxyhx+G/SkJoRuDoIDoZvADqsfllDV4ouZHFZzEeLK5pxWKa9T4+KcGcr3SliJ/Oo1akzSvHR+Iv4rbrNkggRQviqZZESsswaVg4qDSCFxAcYipb6T+Yx64gmuvXTstn1tpuC/8SPRAps1hm2KAuYzHCHT3SaWlau18FBeMrXdzsQQQjAU4M3t3NeVRlwjbxgQNQlLT0IR9slEMgIWiC+UukXUTLqfQAn2aV7Sf2SvCzWE2ZoH6VRirHm2p0UZ9lQHl0gQKBgQD8PJmigRTS/youZTK8uigE41KhxH+1JId0tQgXJ+veyKIC5BKyPHu+iukwQdVaSvpKVJIRnX3RgP0Pq6dYGgAj5+T0u7zEmQ51qXwoC3O864Ocpf8mWeBwrC8G5PBMPpDMXewSnDRu7ZxlaiEnVpXCQOjbkrn6c01/Cq9ZNmziGwKBgQDhqENrJh57OvKWPOhyeHxuo90aeL5vYms6QPPXGE0wHwUMAAwAOBtij1l+D/7BRsgWyxeHi9RPa0Dw6vCHdWbFtX+S0HvUYu87pLXZfqvyTUplLIuR7vVUY9w+iBcAIf39gAazmb9tcp7TKfFOhYUvA/Rd4vBHHMCqKA0hxnzfUQKBgEncmQScFfP/YmO5oJThUky0IINcXiU2MDA9vsPBHfHcAI0mKk9lJLtURBqsfIR3Rjmz8B4WZzyNMA2Br3w+COJ6ynHb89gCFdjgq4myaM7ejGi3tzKMyQtVcGPtcGxzOcWOj02XzkbZb53G6TRyx2HdzhJWMf2j/AKCp3xRUqsXAoGBAJjMkEJIwJEfHE7BFYEVtuRQTxJyujjAAu2iN7rpeLLh/k/e5EFtvJVjjey/tfh4Dlro0Jj3BR9197Zqz85Zn+q2eJkYVRE+oNHZOU356UKcNYkGvcdmcHbhW6x4CGMNoWf7Pg51SFs7JvLDzuMlpEZuvhSo65yrioZtsl6xgbLRAoGBAJyeksCNyZ7b1GT0cXwvL4mtx+3nk+BWWQOAqqkWPRe6UBdL5wiGyYnSbou9FJ3VOCpfX9eqOookjAKCwUkTv2MUvBLzCYYUbtTIjHtTzM2gpD+RflZ8UGqWVuXUr/NDRuMw9mSSHgXUOTxLcTrlOa5NXHZtVhMTR/NNFKrGV5Ye'
+console.log('myPrivateKey:', myPrivateKey)
+console.log('myPublicKey:', myPublicKey)
+
 // 第一步加密 - 用你的公钥加密
+rsa.setPublicKey(yourPublicKey)
+const encryptMsg = rsa.encrypt(msg)
+console.log('加密 - 用你的公钥加密:', encryptMsg)
 // 第二步签名 - 用我的私钥签名
+rsa.setPrivateKey(myPrivateKey)
+const signature = rsa.sign(encryptMsg as string, CryptoJS.SHA256, 'sha256')
+console.log('签名 - 用我的私钥签名:', signature)
 // 第三步发送 - 签名的消息和我的公钥
 rsa.setPrivateKey(myPrivateKey)
-const signature = rsa.sign(msg, CryptoJS.SHA256, 'sha256')
-console.log('signature:', signature)
+// 签字
+rsa.setPublicKey(myPublicKey)
+const verify = rsa.verify(encryptMsg as string, signature as string, CryptoJS.SHA256)
+console.log('认签 - 用我的公钥认签:', verify ? '通过' : '未通过')
+// 解密 - 用你的秘钥解密
+rsa.setPrivateKey(yourPrivateKey)
+const result = rsa.decrypt(encryptMsg as string)
+console.log('解密 - 用你的私钥解密，解密结果：', result)
+
 const form = ref({
   name: '',
   password: '',
